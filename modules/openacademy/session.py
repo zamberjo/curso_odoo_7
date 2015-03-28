10d67c2359cf185d98e576864d2f914d9a893b83
root <root@odoocurso>
2015-03-28 12:23:49 +0100

    backup...

--- modules/openacademy/session.py
+++ modules/openacademy/session.py
@@ -15,6 +15,39 @@ class openacademy_session(orm.Model):
     _order = 'date_start'
     _date_name = 'date_start'
 
+    def _search_by_taken_seats(self, cr, uid, model, field_name, criterion,
+                               context=None):
+        """
+            TODO::
+        """
+        # pdb.set_trace()
+        session_ids = self.search(cr, uid, [])
+        session_ids_founded = []
+        for criteria in criterion:
+            for record in self.browse(cr, uid, session_ids, context=context):
+                if criteria[0] == 'taken_seats_pct':
+                    if criteria[1] == '=':
+                        if criteria[2] == record.taken_seats_pct:
+                            session_ids_founded += [record.id]
+                    elif criteria[1] == '>=':
+                        if criteria[2] >= record.taken_seats_pct:
+                            session_ids_founded += [record.id]
+                    elif criteria[1] == '<=':
+                        if criteria[2] >= record.taken_seats_pct:
+                            session_ids_founded += [record.id]
+        return [('id', 'in', session_ids_founded)]
+
+    def _get_taken_seats_pct(self, cr, uid, ids, field_name, args,
+                             context=None):
+        """
+            TODO::
+        """
+        res = {}
+        for record in self.browse(cr, uid, ids, context=context):
+            res[record.id] = record.seats \
+                and 100.0 * len(record.attendee_ids) / record.seats
+        return res
+
     def _get_date_end(self, cr, uid, ids, field_name, args, context=None):
         """
             TODO::
@@ -44,6 +77,16 @@ class openacademy_session(orm.Model):
             _get_date_end, type="date", string="End date", store=True),
         'duration': fields.float('Duration', help='Duration in days'),
         'seats': fields.integer('Number of Seats'),
+
+
+        'taken_seats_pct': fields.function(
+            _get_taken_seats_pct, type="float",
+            fnct_search=_search_by_taken_seats,
+            string="Taken Seats",
+            help="Percentage of taken seats"),
+
+
+
         'active': fields.boolean('Active'),
         'state': fields.selection(
             [

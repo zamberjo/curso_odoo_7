3c597b2459e4b6786181dfd0cb514548c5bd07ce
root <root@odoocurso>
2015-03-28 10:51:03 +0100

    backup...

--- modules/openacademy/session.py
+++ modules/openacademy/session.py
@@ -1,7 +1,12 @@
 # -*- encoding: utf-8 -*-
 
+# Debugger
+import pdb
 # Necesario para definir los modelos de nuestro módulo OpenERP y sus fields.
 from osv import orm, fields
+# Fechas
+import datetime
+from tools import DEFAULT_SERVER_DATE_FORMAT as DEF_DATE
 
 
 class openacademy_session(orm.Model):
@@ -10,9 +15,33 @@ class openacademy_session(orm.Model):
     _order = 'date_start'
     _date_name = 'date_start'
 
+    def _get_date_end(self, cr, uid, ids, field_name, args, context=None):
+        """
+            TODO::
+            :param list ids: Lista de ID de los registros del modelo.
+
+        """
+        # pdb.set_trace()
+        res = {}
+        for record in self.browse(cr, uid, ids, context=context):
+            if record.date_start:
+                date_start_datetime = datetime.datetime.strptime(
+                    record.date_start, DEF_DATE)
+
+                if record.duration:
+                    days = datetime.timedelta(record.duration, seconds=-1)
+                    date_end = date_start_datetime + days
+                else:
+                    date_end = date_start_datetime
+
+                res[record.id] = date_end.strftime(DEF_DATE)
+        return res
+
     _columns = {
         'name': fields.char('Title', size=128, required=True),
         'date_start': fields.date('Start Date'),
+        'date_end': fields.function(
+            _get_date_end, type="date", string="End date", store=True),
         'duration': fields.float('Duration', help='Duration in days'),
         'seats': fields.integer('Number of Seats'),
         'active': fields.boolean('Active'),

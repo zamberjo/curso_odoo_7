66378b477e0c55b7b73dffaa83a4239af6ae11ca
root <root@odoocurso>
2015-03-28 13:15:41 +0100

    backup...

--- modules/openacademy/course.py
+++ modules/openacademy/course.py
@@ -2,19 +2,54 @@
 
 # Necesario para definir los modelos de nuestro módulo OpenERP y sus fields.
 from osv import orm, fields
-
+# Debugger
 import pdb
+# Openerp
+from openerp import tools
 
 
 class openacademy_course(orm.Model):
     _name = 'openacademy.course'
     _description = 'Course'
 
+    def _set_image(self, cr, uid, id, field_name, field_value, arg,
+                   context=None):
+        """
+            TODO::
+        """
+        pdb.set_trace()
+        return self.write(cr, uid, [id], {
+            'image': tools.image_resize_image_big(field_value),
+        }, context=context)
+
+    def _get_image(self, cr, uid, ids, field_name, arg, context=None):
+        """
+            TODO::
+        """
+        res = {}
+        for record in self.browse(cr, uid, ids, context=context):
+            res[record.id] = tools.image_get_resized_images(
+                record.image, avoid_resize_medium=True)
+        return res
+
     _columns = {
         'image': fields.binary(
             string="Image",
             help="This field holds the image used as image for the product, "
                  "limited to 1024x1024px."),
+
+        'image_medium': fields.function(
+            _get_image, fnct_inv=_set_image, type="binary", multi="image",
+            help="This field holds...128x128px", string="Image", store={
+                'openacademy.course': (lambda s,cr,u,ids,c: ids, ['image'], 10),
+            }),
+
+        'image_small': fields.function(
+            _get_image, fnct_inv=_set_image, type="binary", multi="image",
+            help="This field holds...64x64px", string="Image", store={
+                'openacademy.course': (lambda s,cr,u,ids,c: ids, ['image'], 10),
+            }),
+
         'name': fields.char('Title', size=128, translate=True, required=True),
         'description': fields.text('Description', translate=True),
         'active': fields.boolean('Active'),
